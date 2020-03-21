af13d7ac44186bc7105746e8a8be4786ef4e6e7d
Leo <icpc0928@gmail.com>
2020-03-21 16:04:52 +0800

    2

--- app/src/main/java/tw/org/iii/leo/leo15/MainActivity.java
+++ app/src/main/java/tw/org/iii/leo/leo15/MainActivity.java
@@ -4,16 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
+        private  RequestQueue queue;
+        private TextView tv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = findViewById(R.id.tv);
+        queue = Volley.newRequestQueue(this);
+
+
     }
 
+    //四個參數 找volley 的request
     public void text1(View view) {
+
+        StringRequest request = new StringRequest(Request.Method.GET, "https://www.bradchao.com/wp", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                tv.setText(response);
+
+            }
+        },null);
+        queue.add(request);
 
     }
 }
